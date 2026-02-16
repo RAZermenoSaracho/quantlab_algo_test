4154ed39b37d32652e5b78e25a91a17aac2a36a2
RICARDO A. ZERMEÑO SARACHO <137812503+RAZermenoSaracho@users.noreply.github.com>
2026-02-16 02:09:49 +0000

    First commit

---
+++ algo_test1.py
@@ -0,0 +1,21 @@
+prices = []
+
+def generate_signal(candle):
+    global prices
+
+    close_price = candle["close"]
+    prices.append(close_price)
+
+    # Wait until we have enough data
+    if len(prices) < 5:
+        return "HOLD"
+
+    # Simple moving average
+    sma = sum(prices[-5:]) / 5
+
+    if close_price > sma:
+        return "BUY"
+    elif close_price < sma:
+        return "SELL"
+    else:
+        return "HOLD"
